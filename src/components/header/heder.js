28e39cc8d0872bb78886d061decfbda1ff52d273
11rlarkdals <11rlarkdals@gamil.com>
2024-03-27 18:27:17 +0900

    first commit

---
+++ src/components/header/heder.js
@@ -0,0 +1,21 @@
+
+import './header.css';
+
+function Header() {
+  return <div className='header'>
+   <div className='logo'>
+    <span className='big'>web</span>
+   <span>site</span>
+   </div>
+   <nav className='nav'>
+    <div >header</div>
+    <div>slides</div>
+    <div>banner</div>
+    <div>contents</div>
+    <div>footer</div>
+   </nav>
+   <div className='login'>로그인</div>
+        </div>
+}
+
+export default Header;
---
+++ src/components/test.js
@@ -0,0 +1,7 @@
+function Test() {
+    return <div > 
+    <img src="src/tst.jpeg" alt="pic"/>
+    </div>
+  }
+  
+  export default Test;
